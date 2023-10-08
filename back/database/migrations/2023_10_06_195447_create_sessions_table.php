08038660909eff931e6cabe252a0d7510e2ef125
ratmans <ratma96@gmail.com>
2023-10-08 09:51:05 +0000

    vue gestion des sessions

---
+++ back/database/migrations/2023_10_06_195447_create_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Course;
+use App\Models\Salle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Course::class);
+            $table->date("date");
+            $table->foreignIdFor(Salle::class);
+            $table->boolean("valider")->default("true");
+            $table->boolean("renvoyer")->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
